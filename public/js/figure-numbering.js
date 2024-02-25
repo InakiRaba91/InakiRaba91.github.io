078d9ce403145322bcb968816f510a662572d1ca
InakiRaba91 <lanza241@gmail.com>
2024-02-25 21:57:55 +0100

    remove public folder

--- public/js/figure-numbering.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    var figures = document.getElementsByClassName('figure');
-    for (var i = 0; i < figures.length; i++) {
-      var caption = figures[i].getElementsByClassName('caption')[0];
-      caption.innerHTML = '<strong>Figure ' + (i + 1) + '.</strong> ' + caption.innerHTML;
-    }
-  };
